c6c7627cf3d96a3c5b0177e37afe7369fbc43193
FO <119589823+FilipOwsiany@users.noreply.github.com>
2025-05-25 15:39:48 +0200

    feat(baseSemaphore): Added baseSemaphore module

--- components/utility/baseSemaphore/CBaseSemaphore.cpp
+++ components/utility/baseSemaphore/CBaseSemaphore.cpp
@@ -2,3 +2,62 @@
 
 #include "CLogger.h"
 
+CBaseSemaphore::CBaseSemaphore()
+{
+    mSemaphore = xSemaphoreCreateMutex();
+    if (mSemaphore == NULL)
+    {
+        CLogger::log(CLoggerModule::BaseQueue, CLoggerLevel::Error, "Failed to create semaphore");
+    }
+}
+
+CBaseSemaphore::~CBaseSemaphore()
+{
+    if (mSemaphore != NULL)
+    {
+        vSemaphoreDelete(mSemaphore);
+        mSemaphore = NULL;
+    }
+    else
+    {
+        CLogger::log(CLoggerModule::BaseQueue, CLoggerLevel::Error, "Semaphore already deleted or not initialized");
+    }
+}
+
+bool CBaseSemaphore::take(TickType_t blockTime)
+{
+    return xSemaphoreTake(mSemaphore, blockTime) == pdTRUE;
+}
+
+bool CBaseSemaphore::takeFromISR(void)
+{
+    BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+    bool result = xSemaphoreTakeFromISR(mSemaphore, &xHigherPriorityTaskWoken) == pdTRUE;
+
+    // If a higher priority task was woken, yield to it
+    if (xHigherPriorityTaskWoken)
+    {
+        portYIELD_FROM_ISR();
+    }
+
+    return result;
+}
+
+bool CBaseSemaphore::give(void)
+{
+    return xSemaphoreGive(mSemaphore) == pdTRUE;
+}
+
+bool CBaseSemaphore::giveFromISR(void)
+{
+    BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+    bool result = xSemaphoreGiveFromISR(mSemaphore, &xHigherPriorityTaskWoken) == pdTRUE;
+
+    // If a higher priority task was woken, yield to it
+    if (xHigherPriorityTaskWoken)
+    {
+        portYIELD_FROM_ISR();
+    }
+
+    return result;
+}
--- components/utility/baseSemaphore/include/CBaseSemaphore.h
+++ components/utility/baseSemaphore/include/CBaseSemaphore.h
@@ -4,5 +4,17 @@
 #include "commonStd.h"
 #include "commonOs.h"
 
+class CBaseSemaphore
+{
+private:
+    SemaphoreHandle_t mSemaphore;
+public:
+    CBaseSemaphore();
+    ~CBaseSemaphore();
+    bool take(TickType_t blockTime = portMAX_DELAY);
+    bool takeFromISR(void);
+    bool give(void);
+    bool giveFromISR(void);
+};
 
 #endif //__CBASEMUTEX_H__
