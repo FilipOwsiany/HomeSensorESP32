e3a18aa65988b15a2c61f48b9c749e24db722766
FO <119589823+FilipOwsiany@users.noreply.github.com>
2025-06-30 19:15:37 +0200

    feat: Refactor CLoggerHelper to use variadic templates
    Replaces the implementation of CLoggerHelper's logging methods from variadic functions using va_list to variadic templates, enabling type-safe forwarding of arguments. Removes the CLoggerHelper.cpp file and moves all method implementations into the header file.

--- components/utility/logger/include/CLoggerHelper.h
+++ components/utility/logger/include/CLoggerHelper.h
@@ -2,24 +2,63 @@
 #define __CLOGGERHELPER_H__
 
 #include "CLogger.h"
-#include <cstdarg>
 
 class CLoggerHelper
 {
 private:
     CLoggerConfig::CLogModule mModule;
-public:
-    CLoggerHelper(CLoggerConfig::CLogModule aModule);
-    ~CLoggerHelper() = default;
 
-    void criticalFailure(const char* format, ...);
-    void error(const char* format, ...);
-    void warning(const char* format, ...);
-    void success(const char* format, ...);
-    void performance(const char* format, ...);
-    void importantInfo(const char* format, ...);
-    void info(const char* format, ...);
-    void debug(const char* format, ...);
+public:
+    CLoggerHelper(CLoggerConfig::CLogModule aModule)
+        : mModule(aModule) {}
+
+    template<typename... Args>
+    void criticalFailure(const char* format, Args... args)
+    {
+        CLogger::log(mModule, CLoggerConfig::CLogLevel::CriticalFailure, format, args...);
+    }
+
+    template<typename... Args>
+    void error(const char* format, Args... args)
+    {
+        CLogger::log(mModule, CLoggerConfig::CLogLevel::Error, format, args...);
+    }
+
+    template<typename... Args>
+    void warning(const char* format, Args... args)
+    {
+        CLogger::log(mModule, CLoggerConfig::CLogLevel::Warning, format, args...);
+    }
+
+    template<typename... Args>
+    void success(const char* format, Args... args)
+    {
+        CLogger::log(mModule, CLoggerConfig::CLogLevel::Success, format, args...);
+    }
+
+    template<typename... Args>
+    void performance(const char* format, Args... args)
+    {
+        CLogger::log(mModule, CLoggerConfig::CLogLevel::Performance, format, args...);
+    }
+
+    template<typename... Args>
+    void importantInfo(const char* format, Args... args)
+    {
+        CLogger::log(mModule, CLoggerConfig::CLogLevel::ImportantInfo, format, args...);
+    }
+
+    template<typename... Args>
+    void info(const char* format, Args... args)
+    {
+        CLogger::log(mModule, CLoggerConfig::CLogLevel::Info, format, args...);
+    }
+
+    template<typename... Args>
+    void debug(const char* format, Args... args)
+    {
+        CLogger::log(mModule, CLoggerConfig::CLogLevel::Debug, format, args...);
+    }
 };
 
 #endif // __CLOGGERHELPER_H__
